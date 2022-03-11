8ceb431a774d128a99486e13014d92a20ec387fb
Tom Vincent <TomVincentCode@gmail.com>
2022-03-11 21:25:21 +0000

    Initial commit

---
+++ sweep_example.py
@@ -0,0 +1,46 @@
+"""
+=============
+Sweep example
+=============
+
+A script that uses `waveshapes`  to play a sweep between two frequencies,
+specified by `freq_sweep_endpoints`, which lasts `duration` seconds and fades
+both in and out.
+
+I wrote this script before I wrote the module, to function as a template for
+how a `waveshapes` script should be called.
+
+Once this script behaves as expected it can be safely deleted, or repurposed as
+an example script for documentation.
+"""
+import numpy as np
+from waveshapes import Scene
+
+# Input parameters for frequency sweep
+chunk_size = 64  # samples
+sample_rate = 44100  # Hz
+duration = 5  # s
+max_volume = 0.5  # fraction of maximum output amplitude
+freq_sweep_endpoints = 880, 440  # Hz
+
+# Generate frequencies and amplitude envelope for every sample
+freq = np.geomspace(*freq_sweep_endpoints, duration * sample_rate)
+t = np.linspace(0, duration, duration * sample_rate, endpoint=False)
+amp = max_volume * np.sin(np.pi * t / duration) ** 2
+
+# Create audio scene
+scene = Scene(chunk_size=chunk_size, sample_rate=sample_rate)
+
+# Add a stereo output with an amp node connected to both inputs
+output = scene.add_dac_stereo()
+amp = scene.add_product(outputs=(output.inputs["R"], output.inputs["L"]))
+
+# Create the oscillator to play the sweep
+osc = scene.add_sine_osc(outputs=amp.inputs.new())
+
+# Create readers to pass data from arrays to the oscillator and amp node
+osc_freq = scene.add_samplewise_reader(array=freq, outputs=osc.inputs["freq"])
+osc_amp = scene.add_samplewise_reader(array=amp, outputs=amp.inputs.new())
+
+# Run the scene and stop after `duration` seconds
+scene.run(duration=duration)
